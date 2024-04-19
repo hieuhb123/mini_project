196e4de03bfc59da2628edee9ff0bd29059a4132
hieuhb123 <sieukhoe654321@gmail.com>
2024-04-19 16:12:55 +0700

    1

--- Menuview.java
+++ Menuview.java
@@ -1,7 +1,7 @@
 import java.awt.BorderLayout;
 import java.awt.Font;
 import java.awt.Image;
-import java.awt.image.ImageFilter;
+import java.awt.Graphics;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -16,19 +16,22 @@ public class Menuview extends JFrame{
         this.setTitle("Demonstration of sorting algorithms on array");
         this.setSize(1000, 500);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new BorderLayout());
         this.setLocationRelativeTo(null);
 
         Controller controll = new Controller(this); 
         JMenuBar jMenuBar = new JMenuBar();
 //start_set_icon
     ImageIcon webIcon = new ImageIcon("my_wife.jpg");
-    ImageIcon wIcon = new ImageIcon("tick.png");
+    ImageIcon usageIcon = new ImageIcon("usage.png");
+    ImageIcon aimsIcon = new ImageIcon("aims.png");
+    ImageIcon quitIcon = new ImageIcon("quit.png");
 //end_set_icon
 
 //start_help
         JMenu jMenu_help = new JMenu("Help");
-        JMenuItem jMenuItemHelp_usage = new JMenuItem("usage", wIcon);
-        JMenuItem jMenuItemHelp_aim = new JMenuItem("aim", wIcon);
+        JMenuItem jMenuItemHelp_usage = new JMenuItem("usage", usageIcon);
+        JMenuItem jMenuItemHelp_aim = new JMenuItem("aim", aimsIcon);
         jMenu_help.add(jMenuItemHelp_usage);
         jMenuItemHelp_usage.addActionListener(controll);
         jMenu_help.addSeparator();
@@ -37,15 +40,14 @@ public class Menuview extends JFrame{
 //end_help
 //start_quit
     JMenu jMenu_quit = new JMenu("Quit");
-    JMenuItem jMenuItemQuit_quit = new JMenuItem("Quit");
+    JMenuItem jMenuItemQuit_quit = new JMenuItem("Quit", quitIcon);
     jMenu_quit.add(jMenuItemQuit_quit);
     jMenuItemQuit_quit.addActionListener(controll);
 //end_quit
 
 //start_set_lable
-        Font font = new Font("Arial", Font.BOLD, 50);
         jLabel = new JLabel();
-        jLabel.setFont(font);
+        jLabel.setFont(new Font("Arial", Font.BOLD, 50));
 //end_set_lable
 
         jMenuBar.add(jMenu_help);
@@ -53,7 +55,6 @@ public class Menuview extends JFrame{
         
         this.setIconImage(webIcon.getImage());
         this.setJMenuBar(jMenuBar);
-        this.setLayout(new BorderLayout());
         this.add(jLabel, BorderLayout.NORTH);
         this.setVisible(true);
     }
