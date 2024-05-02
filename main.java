835c2661ef9d5d66bec3f7d15c887839af1ba275
hieuhb123 <sieukhoe654321@gmail.com>
2024-05-03 01:39:45 +0700

    1

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 import javax.swing.UIManager;
 import view.Menuview;
-public class aims{
+public class main{
     public static void main(String[] args){
         try{
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
